a067c9120a9369a381f2f18a680d02888005fd67
Sebastien <sebastien.pingal@gmail.com>
2025-05-05 16:43:23 +0200

    ✨ add mocked data for Child and Program entities

---
+++ src/utils/mock/mockChild.ts
@@ -0,0 +1,23 @@
+export const mockedJournalEntries: IJournalEntry[] = [
+  {
+    id: '1',
+    date: new Date('2020-01-01'),
+    comment: 'Le petit Thimothé a fait ses devoirs de mathématiques, il a appris à compter les nombres jusqu\'à 10.',
+    images: [
+      'https://img.freepik.com/photos-gratuite/planification-livres-conversation-planification-participation-universite_1418-94.jpg',
+      'https://img.freepik.com/photos-gratuite/concept-education-etudiant-etudiant-faisant-remue-meninges-concept-du-campus-gros-plan-etudiants-qui-discutent-leur-sujet-livres-manuels-scolaires-mise-au-point-selective_1418-645.jpg',
+    ],
+    validatedElements: [],
+  },
+]
+
+export const mockedChild = ref<IChild>({
+  id: '1',
+  firstName: 'Thimothé',
+  name: 'Benjaoui',
+  birthDate: new Date('2020-01-01'),
+  age: 5,
+  gender: 'MALE',
+  program: mockedProgram,
+  journalEntries: mockedJournalEntries,
+})
