e47edaba21e3b4b0a55383fe2e92346377e3f8ef
Sebastien <sebastien.pingal@gmail.com>
2025-05-08 16:57:18 +0200

    🔧 used Card component, make the dom updates when an image is uploaded

--- src/components/JournalEntry.vue
+++ src/components/JournalEntry.vue
@@ -24,118 +24,121 @@ const {
 function onUploadImage(event: FileUploadSelectEvent) {
   if (import.meta.env.DEV) {
     const files = Array.isArray(event.files) ? event.files : [event.files]
+    const newImages = [...entry.value.images]
     for (const file of files) {
       const url = URL.createObjectURL(file)
-      entry.value.images.push(url)
-      toast.add({
-        severity: 'info',
-        summary: 'Image ajoutée',
-        detail: 'L\'image a été ajoutée à la liste des images',
-      })
+      newImages.push(url)
     }
+    // This will trigger the model update and reactivity
+    entry.value = { ...entry.value, images: newImages }
+    toast.add({
+      severity: 'info',
+      summary: '️Image ajoutée',
+      detail: 'L\'image a été ajoutée à la liste des images',
+    })
   }
 }
 </script>
 
 <template>
-  <div class="rounded-lg border p-4 shadow-sm">
-    <div class="flex items-center justify-between">
-      <h3 class="flex items-center font-medium">
-        <i class="i-ci-calendar mr-1 shrink-0" />
-        <Inplace @open="() => onEditOpenDate(entry.date)">
-          <template #display>
-            {{ useDateFormatter(entry.date) }}
-          </template>
-          <template #content="{ closeCallback }">
-            <div class="flex gap-3">
-              <DatePicker
-                v-model="editingDate"
-                class="w-full"
-              />
-              <div class="flex justify-end gap-2">
-                <Button
-                  icon="i-ci-close"
-                  label="Annuler"
-                  severity="secondary"
-                  text
-                  @click="() => onCancelDate(entry.date, closeCallback)"
-                />
-                <Button
-                  icon="i-ci-save"
-                  label="Enregistrer"
-                  severity="primary"
-                  @click="() => onSaveDate(v => entry.date = v, closeCallback)"
-                />
+  <Card>
+    <template #title>
+      <div class="flex items-center justify-between">
+        <h3 class="flex items-center font-medium">
+          <i class="i-ci-calendar mr-1 shrink-0" />
+          <Inplace @open="() => onEditOpenDate(entry.date)">
+            <template #display>
+              {{ useDateFormatter(entry.date) }}
+            </template>
+            <template #content="{ closeCallback }">
+              <div class="flex gap-3">
+                <DatePicker v-model="editingDate" class="w-full" />
+                <div class="flex justify-end gap-2">
+                  <Button
+                    icon="i-ci-close"
+                    label="Annuler"
+                    severity="secondary"
+                    text
+                    @click="() => onCancelDate(entry.date, closeCallback)"
+                  />
+                  <Button
+                    icon="i-ci-save"
+                    label="Enregistrer"
+                    severity="primary"
+                    @click="() => onSaveDate(v => entry.date = v, closeCallback)"
+                  />
+                </div>
               </div>
-            </div>
-          </template>
-        </Inplace>
-      </h3>
-
+            </template>
+          </Inplace>
+        </h3>
+      </div>
       <span class="text-sm text-gray-500">{{ entry.validatedElements.length }} éléments validés</span>
-    </div>
-    <Inplace @open="() => onEditOpenComment(entry.comment)">
-      <template #display>
-        <p>{{ entry.comment }}</p>
-      </template>
-      <template #content="{ closeCallback }">
-        <div class="flex w-full flex-col gap-3">
-          <Textarea
-            v-model="editingComment"
-            auto-resize
-            class="w-full"
-            placeholder="Ajoutez votre commentaire ici..."
-          />
-          <div class="flex justify-end gap-2">
-            <Button
-              class="rounded-lg"
-              icon="i-ci-close"
-              label="Annuler"
-              severity="secondary"
-              text
-              @click="() => onCancelComment(entry.comment, closeCallback)"
-            />
-            <Button
-              class="rounded-lg text-white"
-              icon="i-ci-save"
-              label="Enregistrer"
-              severity="primary"
-              @click="() => onSaveComment(v => entry.comment = v, closeCallback)"
-            />
-          </div>
-        </div>
-      </template>
-    </Inplace>
+    </template>
 
-    <div class="flex flex-wrap items-center gap-2">
-      <template v-if="entry.images.length">
-        <div v-for="(image, index) in entry.images" :key="index" class="relative">
-          <div class="size-24 overflow-hidden rounded-lg">
-            <Image
-              alt="Journal image"
-              class="size-full"
-              image-class="size-full object-cover"
-              preview
-              :src="image"
+    <template #content>
+      <Inplace @open="() => onEditOpenComment(entry.comment)">
+        <template #display>
+          <p>{{ entry.comment }}</p>
+        </template>
+        <template #content="{ closeCallback }">
+          <div class="flex w-full flex-col gap-3">
+            <Textarea
+              v-model="editingComment"
+              auto-resize
+              class="w-full"
+              placeholder="Ajoutez votre commentaire ici..."
+            />
+            <div class="flex justify-end gap-2">
+              <Button
+                class="rounded-lg"
+                icon="i-ci-close"
+                label="Annuler"
+                severity="secondary"
+                text
+                @click="() => onCancelComment(entry.comment, closeCallback)"
+              />
+              <Button
+                class="rounded-lg text-white"
+                icon="i-ci-save"
+                label="Enregistrer"
+                severity="primary"
+                @click="() => onSaveComment(v => entry.comment = v, closeCallback)"
+              />
+            </div>
+          </div>
+        </template>
+      </Inplace>
+
+      <div class="flex flex-wrap items-center gap-2">
+        <template v-if="entry.images.length">
+          <div v-for="(image, index) in entry.images" :key="index" class="relative">
+            <div class="size-24 overflow-hidden rounded-lg">
+              <Image
+                alt="Journal image"
+                class="size-full"
+                image-class="size-full object-cover"
+                preview
+                :src="image"
+              />
+            </div>
+            <Button
+              aria-label="Supprimer l'image"
+              class="absolute -right-2 -top-2 size-8 rounded-full"
+              icon="i-ci-trash-empty"
+              severity="danger"
+              @click="entry.images.splice(index, 1)"
             />
           </div>
-          <Button
-            aria-label="Supprimer l'image"
-            class="absolute -right-2 -top-2 size-8 rounded-full"
-            icon="i-ci-trash-empty"
-            severity="danger"
-            @click="entry.images.splice(index, 1)"
-          />
-        </div>
-      </template>
-      <FileUpload
-        v-model="entry.images"
-        accept="image/*"
-        auto
-        choose-label="Ajouter"
-        mode="basic"
-        @select="onUploadImage"
-      />
-    </div>
-  </div>
+        </template>
+        <FileUpload
+          accept="image/*"
+          auto
+          choose-label="Ajouter"
+          mode="basic"
+          @select="onUploadImage"
+        />
+      </div>
+    </template>
+  </Card>
 </template>
