d5c838ae99726ef266c90b6c75b846cb79a03cde
whiterqbbit <whiterqbbit@proton.me>
2025-05-12 09:41:15 +0200

    🔧 fix mocks type

--- src/utils/mock/mockProgram.ts
+++ src/utils/mock/mockProgram.ts
@@ -2,6 +2,10 @@ const mockedProgramElement: IProgramElement = {
   id: '1',
   name: 'Mathématiques',
   description: 'Mathématiques',
+  journalEntries: [],
+  program: {} as IProgram, // should be mockedProgram but it would create a circular dependency
+  createdAt: new Date('2020-01-01'),
+  updatedAt: new Date('2020-01-01'),
 }
 
 export const mockedProgram: IProgram = {
@@ -9,4 +13,7 @@ export const mockedProgram: IProgram = {
   name: 'CP',
   grade: 'CP',
   elements: [mockedProgramElement],
+  children: [],
+  createdAt: new Date('2020-01-01'),
+  updatedAt: new Date('2020-01-01'),
 }
