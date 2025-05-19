2747270e1f495fb015b5e3f64c1b7c814d80f522
Sebastien <sebastien.pingal@gmail.com>
2025-05-19 12:40:40 +0200

    ♻️ use pinia-plugin-persistedstate instead of persisting manualy

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,29 +2,18 @@ import { defineStore } from 'pinia'
 
 const api = useApi()
 export const useUserStore = defineStore('user', () => {
-  const token = ref(localStorage.getItem('token') || '')
-  const user = ref<IUser | null>(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '') : null)
+  const token = ref('')
+  const user = ref<IUser | null>(null)
 
   watch(token, async (newToken) => {
     if (newToken) {
-      localStorage.setItem('token', newToken)
       const response = await api.auth.me()
       user.value = response.data.value
     }
     else {
-      localStorage.removeItem('token')
       user.value = null
     }
   })
 
-  watch(user, async (newUser) => {
-    if (newUser) {
-      localStorage.setItem('user', JSON.stringify(newUser))
-    }
-    else {
-      localStorage.removeItem('user')
-    }
-  })
-
   return { token, user }
-})
+}, { persist: true })
