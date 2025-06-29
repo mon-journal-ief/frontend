9bbf4f803dc0794500da2f15928c95ff00d8069b
whiterqbbit <whiterqbbit@proton.me>
2025-06-29 02:40:56 +0200

    ✨ (child) add skeleton and basic `ChildCard.vue`

---
+++ src/components/ChildCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const props = defineProps<{
+  child: IChild
+}>()
+
+const child = computed(() => props.child)
+const placeholderImage = computed(() => {
+  return child.value.gender === 'MALE' ? 'https://static.vecteezy.com/system/resources/thumbnails/056/866/270/small/child-with-curly-hair-and-blue-eyes-wearing-a-navy-jacket-against-a-dark-background-in-an-indoor-setting-photo.jpeg' : 'https://img.freepik.com/free-photo/portrait-cute-little-girl-with-long-hair-standing-park_23-2147893135.jpg?w=360'
+})
+</script>
+
+<template>
+  <Card>
+    <template #content>
+      <div class="flex items-center justify-between">
+        <div class="flex items-center gap-8">
+          <img
+            alt="Avatar enfant"
+            class="size-16 rounded-full border-2 border-slate-100 object-cover dark:border-slate-800 md:size-24 lg:size-32"
+            :src="child.image || placeholderImage"
+          >
+
+          <div class="flex flex-col gap-1">
+            <h1>{{ child.name }} {{ child.lastName }}</h1>
+            <p class="text-lg text-slate-500 dark:text-slate-400">{{ child.program?.grade }} - {{ child.age }} ans</p>
+          </div>
+        </div>
+
+        <div class="flex items-center gap-2">
+          <Button
+            icon="i-ci-edit-pencil-01"
+            rounded
+            severity="secondary"
+            variant="outlined"
+          />
+          <Button
+            icon="i-ci-trash-full"
+            rounded
+            severity="secondary"
+            variant="outlined"
+          />
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
--- src/pages/enfant/[id].vue
+++ src/pages/enfant/[id].vue
@@ -1,58 +1,63 @@
 <script setup lang="ts">
-const childRef = ref<IChild>(mockedChild)
+const api = useApi()
+const route = useRoute()
+const child = ref<IChild>()
+
+onMounted(async () => {
+  const { id } = route.params as { id: string }
+  child.value = await api.children.get(id)
+})
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
-    <Card>
-      <template #title>
-        <div class="flex items-center gap-2">
-          <img
-            v-if="childRef.image"
-            alt="Avatar enfant"
-            class="size-16 rounded-full border-4 object-cover"
-            :class="childRef.gender === 'MALE' ? 'dark:border-blue-800 border-blue-400' : 'dark:border-pink-800 border-pink-400'"
-            :src="childRef.image"
-          >
-          <h1>{{ childRef.firstName }} {{ childRef.name }}</h1>
-        </div>
-      </template>
+  <div v-if="child" class="flex flex-col gap-4">
+    <ChildCard :child />
 
+    <Card>
       <template #content>
-        <div class="flex flex-col gap-1">
-          <p>{{ childRef.age }} ans</p>
-          <p>{{ childRef.program.grade }}</p>
-        </div>
+        <Tabs value="0">
+          <TabList class="-mt-4 mb-8">
+            <Tab value="0">Journal de bord</Tab>
+            <Tab value="1">Programme</Tab>
+          </TabList>
+
+          <!-- Journal de bord -->
+          <TabPanel value="0">
+            <div class="flex flex-col gap-4">
+              <JournalEntry
+                v-for="entry in child.journalEntries"
+                :key="entry.id"
+                :model-value="entry"
+              />
+
+              <div class="flex justify-center">
+                <Button
+                  class="rounded-lg text-white"
+                  icon="i-ci-plus"
+                  severity="secondary"
+                  variant="outlined"
+                />
+              </div>
+            </div>
+          </TabPanel>
+
+          <!-- Programme -->
+          <TabPanel value="1">
+            <ProgramViewer />
+
+            <div class="flex flex-col gap-4">
+              <div class="flex justify-center">
+                <Button
+                  class="rounded-lg text-white"
+                  icon="i-ci-plus"
+                  severity="secondary"
+                  variant="outlined"
+                />
+              </div>
+            </div>
+          </TabPanel>
+        </Tabs>
       </template>
     </Card>
-
-    <div class="flex w-full flex-col gap-4">
-      <Card class="w-full">
-        <template #title>
-          <h2>Journal de bord de {{ childRef.firstName }} </h2>
-        </template>
-
-        <template #content>
-          <div class="flex flex-col gap-4">
-            <JournalEntry
-              v-for="entry in childRef.journalEntries"
-              :key="entry.id"
-              :model-value="entry"
-            />
-            <div class="flex justify-center">
-              <Button
-                class="rounded-lg text-white"
-                icon="i-ci-plus"
-                severity="secondary"
-                variant="outlined"
-              />
-            </div>
-          </div>
-        </template>
-      </Card>
-
-      <ProgramViewer />
-
-    </div>
   </div>
 </template>
