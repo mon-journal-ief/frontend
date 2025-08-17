0bcdfe3996aaeba175b5c55ae37edb3cfdea6799
whiterqbbit <whiterqbbit@proton.me>
2025-08-17 20:19:52 +0200

    ✨ (debug): remove CRUD tabs, add env, store, and cache debug

--- src/pages/debug.vue
+++ src/pages/debug.vue
@@ -1,18 +1,63 @@
+<script setup lang="ts">
+const userStore = useUserStore()
+
+const user = computed(() => userStore.user)
+
+const env = computed(() => import.meta.env)
+
+function clearLocalStorage() {
+  localStorage.clear()
+  toast.success('Local storage cleared', 'Local storage has been cleared')
+}
+</script>
+
 <template>
-  <Tabs value="1">
-    <TabList>
-      <Tab value="0">Program</Tab>
-      <Tab value="1">ProgramElement</Tab>
-    </TabList>
+  <div class="flex flex-col gap-4 overflow-auto">
+    <div class="flex gap-4">
+      <!-- ACTIONS -->
+      <Card>
+        <template #title>
+          Actions
+        </template>
+        <template #content>
+          <Card class="w-full">
+            <template #content>
+              <div class="flex items-center justify-between gap-4">
+                <label for="account_enabled">Clear local storage</label>
 
-    <!-- PROGRAM -->
-    <TabPanel value="0">
-      <TabDebugProgram />
-    </TabPanel>
+                <Button
+                  icon="i-ci-trash-empty"
+                  label="Clear"
+                  outlined
+                  severity="secondary"
+                  size="small"
+                  @click="clearLocalStorage"
+                />
+              </div>
+            </template>
+          </Card>
+        </template>
+      </Card>
 
-    <!-- ELEMENT -->
-    <TabPanel value="1">
-      <TabDebugProgramElement />
-    </TabPanel>
-  </Tabs>
+      <!-- ENV -->
+      <Card>
+        <template #title>
+          Environment
+        </template>
+        <template #content>
+          <pre class="text-sm">{{ env }}</pre>
+        </template>
+      </Card>
+    </div>
+
+    <!-- USER INFO -->
+    <Card>
+      <template #title>
+        User Information
+      </template>
+      <template #content>
+        <pre class="overflow-auto text-sm">{{ user }}</pre>
+      </template>
+    </Card>
+  </div>
 </template>
