c7c851fc9140158d67b6d315ff8379235c96fbb4
whiterqbbit <whiterqbbit@proton.me>
2025-08-23 17:25:02 +0200

    ✨ (`ButtonDarkMode`): remove "system" option

--- src/components/ButtonDarkMode.vue
+++ src/components/ButtonDarkMode.vue
@@ -2,59 +2,26 @@
 import { useColorMode } from '@vueuse/core'
 
 const colorMode = useColorMode()
-const menu = ref()
 
 const themeIcons = {
   light: 'i-ci-sun',
   dark: 'i-ci-moon',
-  system: 'i-ci-monitor',
 }
-
-const items = ref([
-  {
-    label: 'Light',
-    icon: themeIcons.light,
-    command: () => colorMode.value = 'light',
-  },
-  {
-    label: 'Dark',
-    icon: themeIcons.dark,
-    command: () => colorMode.value = 'dark',
-  },
-  {
-    label: 'System',
-    icon: themeIcons.system,
-    command: () => colorMode.value = 'auto',
-  },
-])
 
 const iconClass = computed(() => {
   if (colorMode.store.value === 'light') return themeIcons.light
   if (colorMode.store.value === 'dark') return themeIcons.dark
 
-  return themeIcons.system
+  return themeIcons.light
 })
-
-function toggleMenu(event: MouseEvent) {
-  menu.value.toggle(event)
-}
 </script>
 
 <template>
   <Button
-    aria-controls="theme_menu"
-    aria-haspopup="true"
     :icon="iconClass"
     outlined
     rounded
     severity="secondary"
-    @click="toggleMenu"
-  />
-  <Menu
-    id="theme_menu"
-    ref="menu"
-    class="mt-2"
-    :model="items"
-    popup
+    @click="colorMode = colorMode === 'light' ? 'dark' : 'light'"
   />
 </template>
