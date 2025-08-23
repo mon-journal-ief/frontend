d98925db4dee31974440295ca1b3270e47c773ac
whiterqbbit <whiterqbbit@proton.me>
2025-08-23 17:25:53 +0200

    ✨ (toastService): update toast durations depending on severity

--- src/services/toastService.ts
+++ src/services/toastService.ts
@@ -4,19 +4,21 @@ export function setToastInstance(toast: ReturnType<typeof useToast>) {
   toastInstance = toast
 }
 
-const DEFAULT_LIFE = 3000
+const DEFAULT_LIFE = 3000 // 3 seconds
+const WARNING_LIFE = 5000 // 5 seconds
+const ERROR_LIFE = 10000 // 10 seconds
 
 export const toast = {
   success: (summary: string, detail: string, life: number = DEFAULT_LIFE) => {
     toastInstance!.add({ severity: 'success', summary, detail, life })
   },
-  error: (summary: string, detail: string, life: number = DEFAULT_LIFE) => {
+  error: (summary: string, detail: string, life: number = ERROR_LIFE) => {
     toastInstance!.add({ severity: 'error', summary, detail, life })
   },
-  warn: (summary: string, detail: string, life: number = DEFAULT_LIFE) => {
+  warn: (summary: string, detail: string, life: number = WARNING_LIFE) => {
     toastInstance!.add({ severity: 'warn', summary, detail, life })
   },
-  info: (summary: string, detail: string, life: number = DEFAULT_LIFE) => {
+  info: (summary: string, detail: string, life: number = DEFAULT_LIFE) => { // TODO: remove this
     toastInstance!.add({ severity: 'info', summary, detail, life })
   },
 }
